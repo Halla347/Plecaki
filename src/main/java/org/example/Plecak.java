bf7115c193be50f3609f2bb7922d7d1289437864
Your Name <you@example.com>
2025-11-21 10:59:57 +0100

    Dodanie klasy

---
+++ src/main/java/org/example/Plecak.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Plecak {
+    private String zawartosc;
+
+    public Plecak(String zawartosc) {
+        this.zawartosc = zawartosc;
+    }
+
+    public String getZawartosc() {
+        return zawartosc;
+    }
+
+    public void setZawartosc(String zawartosc) {
+        this.zawartosc = zawartosc;
+    }
+}
