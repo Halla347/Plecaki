f9f524f0cd7003ab7a37c0db263d4c151bfb5397
Your Name <you@example.com>
2025-11-21 11:53:10 +0100

    Dodanie pakowanie

--- src/main/java/org/example/Plecak.java
+++ src/main/java/org/example/Plecak.java
@@ -16,11 +16,19 @@ public class Plecak {
     }
 
     public String zawartoscKomoryJeden() {
-
         return zawartosc.substring(0,zawartosc.length()/2);
     }
 
     public String zawartoscKomoryDwa() {
         return zawartosc.substring(zawartosc.length()/2);
+    }
+    public char zleSpakowany(){
+        for (int i = 0; i < zawartoscKomoryJeden().length(); i++) {
+            char znak = zawartoscKomoryJeden().charAt(i);
+            if (zawartoscKomoryDwa().contains(String.valueOf(znak))){
+                return znak;
+            }
+        }
+        return 'x';
     }
 }
--- src/test/java/PlecakTest.java
+++ src/test/java/PlecakTest.java
@@ -7,12 +7,40 @@ public class PlecakTest {
     public void zwrociZawartoscKomoryJeden(){
         //given
         String z = "vJrwpWtwJgWrhcsFMMfFFhFp";
+
         Plecak plecak = new Plecak(z);
+
         //when
         String k1 = plecak.zawartoscKomoryJeden();
         String k2 = plecak.zawartoscKomoryDwa();
         //then
         Assertions.assertEquals(k1,"vJrwpWtwJgWr");
         Assertions.assertEquals(k2, "hcsFMMfFFhFp");
+    }
+    @Test
+    public void zwrociZawartoscKomoryDwa(){
+        //given
+        String z = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
+
+        Plecak plecak = new Plecak(z);
+
+        //when
+        String k1 = plecak.zawartoscKomoryJeden();
+        String k2 = plecak.zawartoscKomoryDwa();
+        //then
+        Assertions.assertEquals(k1,"jqHRNqRjqzjGDLGL");
+        Assertions.assertEquals(k2, "rsFMfFZSrLrFZsSL");
+    }
+    @Test
+    public void zwrotZleSpakowangeo(){
+        String z = "vJrwpWtwJgWrhcsFMMfFFhFp";
+
+        Plecak plecak = new Plecak(z);
+
+        //when
+        char element = plecak.zleSpakowany();
+        //then
+        Assertions.assertEquals('p',element);
+
     }
 }
